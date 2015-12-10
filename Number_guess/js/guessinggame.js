f43e10a9fac06f7b96a1d871d814ce2f2a58ab72
andrewmenendez <andrew@engrade.com>
2015-12-09 22:21:31 -0800

    trump-loser

--- Number_guess/js/guessinggame.js
+++ Number_guess/js/guessinggame.js
@@ -1,48 +1,188 @@
 /* **** Global Variables **** */
 // try to elminate these global variables in your project, these are here just to start.
 
-var playersGuess,
-    winningNumber
+//var playersGuess,
 
+winningNumber=generateWinningNumber();
+//playersGuess=playersGuessSubmission();
+playersGuessArray=[];
 
-
+guessLimit=5
 /* **** Guessing Game Functions **** */
 
 // Generate the Winning Number
 
 function generateWinningNumber(){
-	// add code here
+    x=0
+    x=Math.random()
+	x=Math.round(x*100)
+ 	return x;
 }
 
 // Fetch the Players Guess
 
 function playersGuessSubmission(){
 	// add code here
-}
+	 
+		var playersGuess = +$('#user_number').val();
+		//alert(playersGuess);
+		return playersGuess
+	
+};
 
 // Determine if the next guess should be a lower or higher number
 
 function lowerOrHigher(){
 	// add code here
+	distance=Math.abs(winningNumber-curGuess)
+	var direction
+	if(curGuess > winningNumber){
+		direction="high"
+	}else if (curGuess < winningNumber){
+		direction="low"
+	}
+	return [direction,distance]
+}
+
+// The guessMessage function
+
+function guessMessage(){
+	guessData=lowerOrHigher();
+	guessnum=playersGuessArray.length
+	$('#stats').text('Guesses: '+guessnum+"  "+"Last Guess: "+curGuess + "  Remaining: "+(guessLimit-1));
+
+	if (guessData[1] > 20) {
+		hintString2=" You're off by more than 20"
+
+	} else {
+		hintString2=" But you're within 20!"
+	}
+
+	hintString1="Your guess was too " + guessData[0] + ";"
+	
+	console.log(hintString1,hintString2)
+
+	gmes=hintString1+'\n'+hintString2
+	$('#gmessage').text(gmes);
 }
 
 // Check if the Player's Guess is the winning number 
 
 function checkGuess(){
-	// add code here
+	//console.log(curGuess,winningNumber)
+	if (curGuess==winningNumber){
+		$('#instructions').text("Winner!");
+		$('#stats').text('Guesses: '+guessnum+"  "+"Winning Guess: "+curGuess);
+		$('#gmessage').hide()
+		$('#hintmessage').hide()
+		$('#wheel').find('img').attr({"src": "img/trump.jpg","width":300})
+		return true
+	} else if (guessLimit==1){
+		$('#instructions').text("You lost");
+		$('#stats').text('Guesses: '+guessnum+"  "+"Winning number: "+winningNumber);
+		$('#gmessage').hide()
+		$('#hintmessage').hide()
+		$('#wheel').find('img').attr({"src": "img/trump-lose.jpg","width":300})
+	}
+
+	else {
+		guessMessage();
+		$('#instructions').text("Try Again...");
+		return false
+
+	}
+
 }
 
 // Create a provide hint button that provides additional clues to the "Player"
 
 function provideHint(){
 	// add code here
+	fact_array=factors(winningNumber)
+	console.log(fact_array)
+	fact_array.splice(0,1)
+	fact_array.splice(-1,1)
+	console.log(fact_array)
+
+	if (fact_array.length > 1){
+		$('#hintmessage').text("factors include "+fact_array)
+	} else {
+		$('#hintmessage').text("it's a prime number");
+	}
 }
 
 // Allow the "Player" to Play Again
 
 function playAgain(){
-	// add code here
+	winningNumber=generateWinningNumber(); 
+	playersGuessArray=[]
+	$('#hintmessage').text("");
+	$('#instructions').text("Enter a number");
+	$('#gmessage').text("");
+	$('#wheel').find('img').attr({"src": "img/roulette.png","width":200})
+	$('#stats').text("");
+	
+	console.log("new number is ",winningNumber)
 }
 
+//this is unneccessary; I was using it to debug something...
+function setValue(playersGuessArray){
+	curGuess=playersGuessArray[playersGuessArray.length - 1]
+	return curGuess
+}
+
+function factors(num){
+    ints=[]
+    facts=[]
+    for(i=num;i>0;i--){
+	        ints.push(i)
+	    }
+	    //console.log(ints)
+	    for (i in ints){
+	        for (j in ints){
+	            if (ints[i]*ints[j]==num){
+	                facts.push(ints[i]);
+	                //facts.push(ints[j]);
+	            }
+	        }
+	    }
+    return facts
+}// End factors
 
 /* **** Event Listeners/Handlers ****  */
+
+
+
+
+$(document).ready(function() {
+  $('#submit').on('click', function(){
+  	var playersGuess=playersGuessSubmission();
+  	//console.log(playersGuess,winningNumber)
+  	$('#user_number').val("");
+  	playersGuessArray.push(playersGuess)
+
+  	curGuess=setValue(playersGuessArray);
+  	console.log(curGuess)
+
+  	checkGuess();
+  	//guessMessage();
+
+  	guessLimit=(guessLimit-1);
+
+  	});
+
+$('#hint_button').on('click', function(){
+	provideHint(); 
+  });
+
+$('#playagain_button').on('click', function(){
+	playAgain();
+	
+  });
+
+	
+});
+
+
+
+
